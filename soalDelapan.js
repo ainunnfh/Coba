6ab61e6a5a111f52eaf1a63740cc1a69c5d232ed
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-03-22 21:42:28 +0700

    soal tujuh

---
+++ soalDelapan.js
@@ -0,0 +1,19 @@
+/*
+buatkan fungsi menggunakan untuk menunjukkan 10 bilangan prima pertama
+*/
+
+function myFunction() {
+    for (let i = 2; i <= 10; i++) {
+        let isPrime = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            console.log(i);
+        }
+    }
+}
+
